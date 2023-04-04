502b986c1817ac301427c9464540851e5e9b575f
Imtiaj09 <ahmimtiaj9@gmail.com>
2023-04-04 23:12:04 +0600

    Unhandled runtime error has been handlayed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Nunito } from "next/font/google"
 import './globals.css'
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modals";
 
 export const metadata = {
   title: 'Airbnb',
@@ -19,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
+        <ClientOnly>
+          <Modal/>
+          <Navbar/>
+        </ClientOnly>
         {children}
         </body>
     </html>
