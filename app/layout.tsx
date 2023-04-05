d878f396f79c172930a72c129304f2abaa6ad2a6
Imtiaj09 <ahmimtiaj9@gmail.com>
2023-04-06 05:54:54 +0600

    app> provider> ToasterProvider has been created and react-hot-tost package has been installed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+
 
 export const metadata = {
   title: 'Airbnb',
@@ -22,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider/>
           <RegisterModal/>
           <Navbar/>
         </ClientOnly>
@@ -30,3 +33,4 @@ export default function RootLayout({
     </html>
   )
 }
+
